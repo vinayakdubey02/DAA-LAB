03f27507d1f6bb06b702c675ba53704a41aa9585
vinayakdubey02 <dvinayak139@gmail.com>
2026-09-18 11:29:05 +0530

    Updated Lab 4 Q1 Merge Sort

--- Lab 4/Q1.c
+++ Lab 4/Q1.c
@@ -1,71 +1,68 @@
 #include <stdio.h>
 
+void merge(int a[], int low, int mid, int high) {
+    int temp[100];
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+
+    while(i <= mid && j <= high) {
+        if(a[i] <= a[j]) {
+            temp[k] = a[i];
+            i++;
+        }
+        else {
+            temp[k] = a[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i <= mid) {
+        temp[k] = a[i];
+        i++;
+        k++;
+    }
+
+    while(j <= high) {
+        temp[k] = a[j];
+        j++;
+        k++;
+    }
+
+    for(i = low; i <= high; i++) {
+        a[i] = temp[i];
+    }
+}
+
+void mergeSort(int a[], int low, int high) {
+    if(low < high) {
+        int mid = (low + high) / 2;
+
+        mergeSort(a, low, mid);
+        mergeSort(a, mid + 1, high);
+
+        merge(a, low, mid, high);
+    }
+}
+
 int main() {
-    int a[100], temp[100];
-    int n, i, j, k;
-    int low, mid, high;
-    int size;
+    int a[100], n, i;
 
     printf("Enter n: ");
     scanf("%d", &n);
 
     printf("Enter elements: ");
-    for(i = 0; i < n; i++)
+    for(i = 0; i < n; i++) {
         scanf("%d", &a[i]);
-
-    
- for(size = 1; size < n; size = size * 2) {
-
-  for(low = 0; low < n - 1; low = low + 2 * size) {
-
-     mid = low + size - 1;
-         high = low + 2 * size - 1;
-
-       if(mid >= n)
-           mid = n - 1;
-
-        if(high >= n)
-       high = n - 1;
-
-           i = low;
-           j = mid + 1;
-           k = low;
-
-       while(i <= mid && j <= high) {
-
-           if(a[i] < a[j]) {
-          temp[k] = a[i];
-            i++;
-                }
-    else {
-    temp[k] = a[j];
-      j++;
-      }
-
-        k++;
-     }
-
-      while(i <= mid) {
-         temp[k] = a[i];
-         i++;
-          k++;
-            }
-
-          while(j <= high) {
-                temp[k] = a[j];
-                j++;
-                k++;
-            }
-
-            for(i = low; i <= high; i++)
-                a[i] = temp[i];
-        }
     }
 
-    printf("Sorted array: ");
+    mergeSort(a, 0, n - 1);
 
-    for(i = 0; i < n; i++)
+    printf("Sorted array: ");
+    for(i = 0; i < n; i++) {
         printf("%d ", a[i]);
+    }
 
     return 0;
 }
